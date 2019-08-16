2324575a9b0d40d15ef72e30a2300d1002f5acc2
Pablo Sichert <mail@pablosichert.com>
2019-08-16 12:59:30 +0200

    Add dummy app

---
+++ Dummy/Dummy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dummy
+//
+//  Created by Pablo Sichert on 16.08.19.
+//  Copyright © 2019 Pablo Sichert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
